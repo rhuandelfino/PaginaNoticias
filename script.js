ef21c8234b70d48c71d09b2a311bce120c3315f2
rhuandelfino <127902637+rhuandelfino@users.noreply.github.com>
2024-12-10 18:30:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Página de Notícias carregada!");
+    // Aqui você pode adicionar funcionalidades como busca, filtros ou carregamento dinâmico.
+});
